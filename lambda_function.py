937a985a317881b470e46660cdb74d234d4deee4
YU <yoshi_nn_ihsoy@icloud.com>
2019-12-31 01:07:41 +0900

    英語、日本語件名のメールにも対応

--- lambda_function.py
+++ lambda_function.py
@@ -3,8 +3,12 @@ import urllib.parse
 import base64
 import boto3
 import os
+import botocore
+import requests
+import time
 
-s3 = boto3.client('s3')
+s3 = boto3.resource("s3")
+client = s3.meta.client
 
 
 #Lambdaに保存した環境変数を取得
@@ -28,33 +32,47 @@ def lambda_handler(event, context):
 
 
     #バケット取得、キー取得。
-    bucket = event['Records'][0]['s3']['bucket']['name']
-    key = urllib.parse.unquote_plus(event['Records'][0]['s3']['object']['key'], encoding='utf-8')
-
-
+    bucket = event["Records"][0]["s3"]["bucket"]["name"]
+    key = urllib.parse.unquote_plus(event["Records"][0]["s3"]["object"]["key"], encoding="utf-8")
 
     try:
-        #レスポンスを取得するにはS3の読み取り権限がある実行ロールが必要
-        response = s3.get_object(Bucket=bucket, Key=key)
-        mail = response['Body'].read().replace('\n','').replace('\r','')
+        time.sleep(0.2)
+        response = client.get_object(Bucket=bucket,Key=key)
+        mail = response["Body"].read().decode("utf-8")
+
         #"envelope-from="以降の文字列を取得して、”;”でfrom_addressを取得する。
-        from_address = mail.split('envelope-from=')[1].split(';')[0]
-        subject = mail.split('Subject: ')[1].split('To: ')[0]
-        body = base64.b64decode(mail.split('base64')[1].split('--')[0])
-        html_body = base64.b64decode(mail.split('base64')[2].split('--')[0])
+        encoded_subject = mail.split("Subject: ")[1].split("\r\n")[0]
+        subject = ""
+        if("=?utf-8?B?" in encoded_subject):
+            encoded_subject = encoded_subject.split("=?utf-8?B?")[1].split("?=")[0]
+            subject         = (base64.b64decode(encoded_subject)).decode("utf-8")
+        else:
+            subject = encoded_subject
 
 
-    except Exception as e:
-        print(e)
-        print('Error getting object {} from bucket {}. Make sure they exist and your bucket is in the same region as this function.'.format(key, bucket))
-        raise e
+        date            = mail.split("Date: ")[1].split("References:")[0].split("\r\n")[0]
+        to_adress       = mail.split("To: ")[1].split("In-Reply")[0].split("\r\n")[0]
+        from_address    = mail.split("envelope-from=")[1].split(";")[0]
+        encoded_body    = mail.split("\r\n\r\n")[1].split("\r\n")[0]
+
+        try:
+            body        = (base64.b64decode(encoded_body)).decode("utf-8").split("\r\n\r\n")[0]
+        except Exception as e:
+            body        = encoded_body
+
+
+    except botocore.exceptions.ClientError as e:
+        if e.response["Error"]["Code"] == "404":
+            logger.error("The object does not exist.")
+        else:
+            raise e
 
 
 
     body_post_dic["record"]["Subject"]["value"]         = subject
     body_post_dic["record"]["FromEmailAdress"]["value"] = from_address
-    #body_post_dic["record"]["ToEmailAdress"]["value"]   = event["Records"][0]["ses"]["mail"]["commonHeaders"]["to"][0]
-    #body_post_dic["record"]["Timestamp"]["value"]       = event["Records"][0]["ses"]["mail"]["commonHeaders"]["date"]
+    body_post_dic["record"]["ToEmailAdress"]["value"]   = to_adress
+    body_post_dic["record"]["Timestamp"]["value"]       = date
     body_post_dic["record"]["Contents"]["value"]        = body
 
 
@@ -64,8 +82,12 @@ def lambda_handler(event, context):
 
 
 
+
+
+
+
+
 def RecordPost2kintone(url, headers, post_record_dic):
     resp = requests.post(url, data=json.dumps(post_record_dic), headers=headers)
-    #resp = requests.post(url, data=post_record_dic, headers=headers)
     record_data = json.loads(resp.text)
     return record_data
